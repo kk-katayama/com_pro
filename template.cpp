04bfc7433cdf07d334465f090bb3a882273c75ca
kkktym <galapagos4444@gmail.com>
2019-08-29 14:47:37 +0900

    Geometry

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <algorithm>
-#define rep(i,n) for(int i = 0 ; i < n ; ++i)
-#define rep1(i,n) for(int i = 1 ; i <= n ; ++i)
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<=n;++i)
 using namespace std;
 int main()
 {
