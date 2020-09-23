a76e0870fe2211160dea8b7890a25a977f586fb6
kkktym <48704363+kk-katayama@users.noreply.github.com>
2020-09-23 22:49:17 +0900

    Update gcd.cpp

--- Numerical/GCD/lib/gcd.cpp
+++ Numerical/GCD/lib/gcd.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
 // ユークリッドの互除法で最大公約数を求める
 int gcd(int a,int b){
   if(b==0) return a;
@@ -10,17 +7,3 @@ int gcd(int a,int b){
 int lcm(int a,int b){
   return a*b/gcd(a,b);
 }
-
-int main()
-{
-  int a,b;
-  cin >> a >> b;
-  cout << "gcd" << ":" << gcd(a,b) << "\n";
-  cout << "lcm" << ":" << lcm(a,b) << "\n";
-  return 0;
-}
-
-
-
-
-
