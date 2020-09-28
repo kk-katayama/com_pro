47a4ec0802ebc5a834c48249c516de49080e5f92
kkktym <galapagos4444@gmail.com>
2020-09-28 14:41:51 +0900

    add

---
+++ Numerical/Mod/Modint/lib/Modint.cpp
@@ -0,0 +1,52 @@
+// Modint
+// modint<MOD> で宣言
+template<ll MOD>
+struct modint{
+  ll x;
+  ll mod = MOD;
+  modint(ll x=0):x(x%MOD){}
+  modint& operator+=(const modint a){
+    if((x+=a.x)>=MOD) x-=MOD;
+    return *this;
+  }
+  modint& operator-=(const modint a){
+    if((x += MOD-a.x)>=MOD) x-=MOD;
+    return *this;
+  }
+  modint& operator*=(const modint a){
+    (x*=a.x)%=MOD;
+    return *this;
+  }
+  modint operator+(const modint a) const{
+    modint res(*this);
+    return res+=a;
+  }
+  modint operator-(const modint a) const{
+    modint res(*this);
+    return res-=a;
+  }
+  modint operator*(const modint a) const{
+    modint res(*this);
+    return res*=a;
+  }
+  modint pow(ll t) const{
+    if(!t) return 1;
+    modint a = pow(t>>1);
+    a*=a;
+    if(t&1) a*=*this;
+    return a;
+  }
+  
+  //for prime mod
+  modint inv() const{
+    return pow(MOD-2);
+  }
+  modint& operator/=(const modint a){
+    return (*this) *= a.inv();
+  }
+  modint operator/(const modint a) const{
+    modint res(*this);
+    return res/=a;
+  }
+};
+using mint = modint<1000000007>;
